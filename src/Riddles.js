9fccc61cb3c72f85de1623cee9cdbd1177882bcf
Christopher Terrano <mterrano1@yahoo.com>
2022-02-28 16:22:00 -0600

    create useEffect

--- src/Riddles.js
+++ src/Riddles.js
@@ -1,6 +1,13 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 function Riddles() {
+
+    useEffect(() => {
+        fetch('http://localhost:3001/riddles')
+        .then(r => r.json())
+        .then(data => console.log(data))
+    }, [])
+
     return (
         <div>
             <h1>Riddles Page</h1>
