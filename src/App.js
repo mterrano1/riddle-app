22575e48e7994f35f27e31cbd225636d5a10f004
Christopher Terrano <mterrano1@yahoo.com>
2022-02-28 13:21:44 -0600

    create routes in app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './Home';
+import Riddles from './Riddles';
+import Riddle from './Riddle';
+import RiddleForm from './RiddleForm';
+import NavBar from './NavBar';
+import Challenge from './Challenge';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <NavBar />
+      <div className="App">
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/riddles' component={Riddles} />
+          <Route exact path='/riddles/new' component={RiddleForm} />
+          <Route path='/riddles/:id' component={Riddle} />
+          <Route path='/challenge' component={Challenge} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
