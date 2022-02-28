aa5e88cffa3a84fb19b63272f1e834dad48681dd
Christopher Terrano <mterrano1@yahoo.com>
2022-02-28 16:30:42 -0600

    create map for fetch request and pass riddle down as props

--- src/Riddles.js
+++ src/Riddles.js
@@ -1,18 +1,24 @@
 import React, { useEffect, useState } from 'react';
+import Riddle from './Riddle';
 
 function Riddles() {
-    const [riddle, setRiddle] = useState([]);
+    const [riddles, setRiddles] = useState([]);
 
     useEffect(() => {
         fetch('http://localhost:3001/riddles')
         .then(r => r.json())
-        .then(data => setRiddle(data))
+        .then(data => setRiddles(data))
     }, [])
+
+    const riddleList = riddles.map((riddle) => (
+        <Riddle key={riddle.id} riddle={riddle} />
+    ))
 
     return (
         <div>
             <h1>Riddles Page</h1>
             <hr/>
+            <ul>{riddleList}</ul>
         </div>
     );
 }
