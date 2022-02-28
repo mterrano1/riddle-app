7f191093f202fcfdf8397ed856d6c3f243d50ad7
Christopher Terrano <mterrano1@yahoo.com>
2022-02-28 13:17:59 -0600

    create Challenge component

---
+++ src/Challenge.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Challenge() {
+    return (
+        <div>
+            <h1>Challenge Page</h1>
+            <hr/>
+        </div>
+    );
+}
+
+export default Challenge;
