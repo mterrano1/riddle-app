e7f296a8362a0f1095ccce612267fcfc73aa5506
Christopher Terrano <mterrano1@yahoo.com>
2022-02-28 13:16:38 -0600

    create RiddleForm component

---
+++ src/RiddleForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function RiddleForm() {
+    return (
+        <div>
+            <h1>RiddleForm Page</h1>
+            <hr/>
+        </div>
+    );
+}
+
+export default RiddleForm;
