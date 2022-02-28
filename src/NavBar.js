3925a9d515dd51b1e1c7de18f22a649a262e1c98
Christopher Terrano <mterrano1@yahoo.com>
2022-02-28 13:23:12 -0600

    add NavLinks to NavBar

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 function NavBar() {
     return (
         <nav>
-            
+            <NavLink exact to='/'>Home</NavLink>
+            <NavLink exact to='/riddles'>Riddles</NavLink>
+            <NavLink to='/riddles/new'>Add New Riddle</NavLink>
+            <NavLink to='/challenge'>Challenge</NavLink>
         </nav>
     );
 }
