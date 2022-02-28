d89d3432d14c81f1298b91b49573c66b5b28e93b
Christopher Terrano <mterrano1@yahoo.com>
2022-02-28 14:35:25 -0600

    create home content

--- src/Home.js
+++ src/Home.js
@@ -5,6 +5,11 @@ function Home() {
         <div>
             <h1>Home Page</h1>
             <hr/>
+            <h3>Riddle me this:</h3>
+            <p>Do you have what it takes to solve these brain teasers? If so, click the button below!</p>
+            <img src='https://media.giphy.com/media/h0Xez8ow1UOVq/giphy.gif' />
+            <br />
+            <button>Get started</button>
         </div>
     );
 }
