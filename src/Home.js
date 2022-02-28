13a60d58fd5a06276976acf938c6a8900907195b
Christopher Terrano <mterrano1@yahoo.com>
2022-02-28 15:23:39 -0600

    create button link to riddles page

--- src/Home.js
+++ src/Home.js
@@ -1,15 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Home() {
     return (
         <div>
             <h1>Home Page</h1>
             <hr/>
+
             <h3>Riddle me this:</h3>
             <p>Do you have what it takes to solve these brain teasers? If so, click the button below!</p>
             <img src='https://media.giphy.com/media/h0Xez8ow1UOVq/giphy.gif' />
             <br />
-            <button>Get started</button>
+
+            <Link to={'/riddles'}>
+                <button>Get started</button>
+            </Link>
         </div>
     );
 }
