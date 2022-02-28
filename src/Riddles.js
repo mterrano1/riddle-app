6a02b79e27ff500eb983833309a811b0596f044a
Christopher Terrano <mterrano1@yahoo.com>
2022-02-28 13:14:01 -0600

    create Riddles component

---
+++ src/Riddles.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Riddles() {
+    return (
+        <div>
+            <h1>Riddles Page</h1>
+            <hr/>
+        </div>
+    );
+}
+
+export default Riddles;
