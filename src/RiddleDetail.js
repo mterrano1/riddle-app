c3d1e2099ef8c20cc0657866ba56f53337a22355
Christopher Terrano <mterrano1@yahoo.com>
2022-03-03 15:12:14 -0600

    Add RiddleDetail component

---
+++ src/RiddleDetail.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function RiddleDetail() {
+    return (
+        <div>
+            <h1>Riddle Detail Page</h1>
+            <hr/>
+        </div>
+    );
+}
+
+export default RiddleDetail;
