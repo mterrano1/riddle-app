878b88ef5efdcdd11cd4c11b14ba275f0f871a7e
Christopher Terrano <mterrano1@yahoo.com>
2022-03-03 15:15:09 -0600

    rename RiddleDetail component to RiddleAnswer

---
+++ src/RiddleAnswer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function RiddleAnswer() {
+    return (
+        <div>
+            <h1>Riddle Answer Page</h1>
+            <hr/>
+        </div>
+    );
+}
+
+export default RiddleAnswer;
