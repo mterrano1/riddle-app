8e5af08f783234fa2bc9db032fed9cb2411a1ff4
Christopher Terrano <mterrano1@yahoo.com>
2022-03-04 15:06:27 -0600

    create state and content

--- src/RiddleForm.js
+++ src/RiddleForm.js
@@ -1,11 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function RiddleForm() {
+    const [title, setTitle] = useState('');
+    const [question, setQuestion] = useState('');
+    const [hint, setHint] = useState('');
+    const [answer, setAnswer] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log('clicked')
+    }
+
     return (
-        <div>
-            <h1>RiddleForm Page</h1>
-            <hr/>
-        </div>
+        <section className='form'>
+            <h3>Add new riddle!</h3>
+            <form onSubmit={handleSubmit}>
+                <label>Title</label>
+                    <br />
+                    <input type='text' value={title} onChange={e => setTitle(e.target.value)} placeholder='Input title...'/>
+                    <br />
+                    <br />
+                <label>Question</label>
+                    <br />
+                    <input type='text' value={question} onChange={e => setQuestion(e.target.value)} placeholder='Input riddle...'/>
+                    <br />
+                    <br />
+                <label>Hint</label>
+                    <br />
+                    <input type='text' value={hint} onChange={e => setHint(e.target.value)} placeholder='Input hint...'/>
+                    <br />
+                    <br />
+                <label>Answer</label>
+                    <br />
+                    <input type='text' value={answer} onChange={e => setAnswer(e.target.value)} placeholder='Input answer...'/>
+                    <br />
+                    <br />
+                <button>Submit</button>
+            </form>
+        </section>
     );
 }
 
