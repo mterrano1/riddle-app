20ace805b6566b8c43d4e20f153d95506f24cdea
Christopher Terrano <mterrano1@yahoo.com>
2022-03-04 14:08:25 -0600

    add useEffect hook to fetch riddle content and display on page

--- src/RiddleAnswer.js
+++ src/RiddleAnswer.js
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
 function RiddleAnswer() {
+    const [riddle, setRiddle] = useState(null);
     const params = useParams();
-    console.log(params)
+
+    useEffect(() => {
+        fetch(`http://localhost:3001/riddles/${params.id}`)
+        .then(r => r.json())
+        .then(riddleObject => setRiddle(riddleObject))
+    }, [params.id])
+
+    if (!riddle) return <h2>Loading...</h2>
+
     
     return (
-        <div>
-            <h1>Riddle Answer Page</h1>
-            <hr/>
-        </div>
+        <ul className='cards'>
+            <li className= 'card' >
+                <h4>{riddle.title}</h4>
+                <hr />
+                <p>{riddle.question}</p>
+                <hr />
+                <p>Answer: {riddle.answer}</p>
+            </li>
+        </ul>
     );
 }
 
