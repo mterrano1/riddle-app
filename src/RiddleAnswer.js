10cd9ca5e1fd7771071bd669bea913f70009e9d9
Christopher Terrano <mterrano1@yahoo.com>
2022-03-04 13:06:41 -0600

    add useParams hook

--- src/RiddleAnswer.js
+++ src/RiddleAnswer.js
@@ -1,6 +1,10 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 function RiddleAnswer() {
+    const params = useParams();
+    console.log(params)
+    
     return (
         <div>
             <h1>Riddle Answer Page</h1>
