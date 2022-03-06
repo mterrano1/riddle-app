20891c8ef4ce1158931ecc73f665a787032d06b0
Christopher Terrano <mterrano1@yahoo.com>
2022-03-05 22:13:59 -0600

    create useEffect countdown timer and click event button to start challenge

--- src/Challenge.js
+++ src/Challenge.js
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 function Challenge() {
+    const [timeLeft, setTimeLeft] = useState(0);
+
+    useEffect(() => {
+        if (timeLeft === 0) {
+            return;
+        }
+
+        const timer = setTimeout(() => {
+            setTimeLeft(timeLeft => timeLeft - 1);
+        }, 1000);
+
+        return function () {
+            clearTimeout(timer);
+        }
+
+    }, [timeLeft]);
+
+    const handleClick = () => {
+        setTimeLeft(60)
+    }
+
     return (
         <div>
             <h1>Challenge Page</h1>
-            <hr/>
+            <hr />
+            <button onClick={handleClick}>Click to begin</button>
+            {timeLeft ? <h4>{timeLeft} seconds remaining!</h4> : <h4>60 seconds remaining!</h4>}  
         </div>
     );
 }
