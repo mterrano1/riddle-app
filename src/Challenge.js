fa06d3dac93e81499439476ad51c0baf86aeef38
Christopher Terrano <mterrano1@yahoo.com>
2022-03-06 01:42:32 -0600

    create state to show good luck image when button is clicked

--- src/Challenge.js
+++ src/Challenge.js
@@ -4,6 +4,7 @@ import ChallengeQuestions from './ChallengeQuestions';
 function Challenge() {
     const [timeLeft, setTimeLeft] = useState(0);
     const [disableBtn, setDisableBtn] = useState(false);
+    const [goodLuck, setGoodLuck] = useState(false);
 
     useEffect(() => {
         if (timeLeft === 0) {
@@ -21,8 +22,12 @@ function Challenge() {
     }, [timeLeft]);
 
     const handleClick = () => {
-        setTimeLeft(100)
-        setDisableBtn(true)
+        setGoodLuck(true)
+        setTimeout(() => {
+            setTimeLeft(100)
+            setDisableBtn(true)
+            setGoodLuck(false)
+        }, 3000)
     }
 
     const handleTimesUp = timeLeft === 0 ? true : false
@@ -31,9 +36,11 @@ function Challenge() {
         <div>
             <h1>Challenge Page</h1>
             <hr />
+            <p>Click the button below and you'll have 100 seconds to try and answer 10 riddles.</p>
             <button disabled={disableBtn} onClick={handleClick}>Click to begin</button>
             {timeLeft ? <h4>{timeLeft} seconds remaining!</h4> : <h4>100 seconds remaining!</h4>}
-            <ChallengeQuestions handleTimesUp={handleTimesUp} />
+            {goodLuck ? <img src={'https://media.giphy.com/media/10AYkGR9M75nLW/giphy.gif'} /> : ''}
+            {disableBtn ? <ChallengeQuestions handleTimesUp={handleTimesUp} /> : ''}
         </div>
     );
 }
